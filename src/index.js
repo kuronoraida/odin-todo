90a0156c2840e4eaed36b493161d19839cc019f2
kuronoraida <99105705+kuronoraida@users.noreply.github.com>
2023-11-28 14:45:10 +1100

    Add project and todo functions

--- src/index.js
+++ src/index.js
@@ -1,1 +1,31 @@
 import './style.css';
+
+/* definitions */
+let projectList = [];
+
+/* factories */
+function createProject (name) {
+    console.log('creating project');
+
+    name = name;
+    let todoList = [];
+
+    const addTodo = (name) => {
+        todoList.push(createTodo(name));
+    };
+
+    return {name, todoList, addTodo};
+};
+
+function createTodo (name) {
+    console.log('creating todo');
+
+    name =  name;
+
+    return {name};
+};
+
+/* tests */
+projectList.push(createProject('goo'));
+projectList[0].addTodo('gaa');
+console.log(projectList);
